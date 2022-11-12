3aa7b6b6dc77be2cda2893e4843fabf474d8c4f2
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-12 11:42:16 -0300

    feat: add commented import for axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// import axios from 'axios';
+
 import { useEffect } from 'react';
 import { useState } from 'react';
 import useGenericState from './hooks/useGenericState';
@@ -39,34 +41,37 @@ function App() {
     <div className="main-container">
       <header>A retro single page blog</header>
       <form className="form-container" onSubmit={handleSubmitClick}>
-        <input
-          onChange={setGenericState}
-          value={genericState.nameInput}
-          placeholder="Type your name..."
-          name="nameInput"
-          type="text"
-          autoComplete="off"
-        />
-        <textarea
-          autoComplete="off"
-          onChange={setGenericState}
-          value={genericState.commentInput}
-          name="commentInput"
-          placeholder="Type your name..."
-          type="text"
-        />
-        <button type="submit">Send</button>
+        <div>
+          <input
+            onChange={setGenericState}
+            value={genericState.nameInput}
+            placeholder="Type your name..."
+            name="nameInput"
+            type="text"
+            autoComplete="off"
+          />
+            <textarea
+              autoComplete="off"
+              onChange={setGenericState}
+              value={genericState.commentInput}
+              name="commentInput"
+              placeholder="Type something cool that happened in your day :)"
+              type="text"
+              maxLength={140}
+            />
+          <button type="submit">Send</button>
+        </div>
       </form>
-
       {comments?.map((comment) => (
         <div className="comment-container" key={comment.id}>
           <div className="comment-info">
-            <p>{comment.user}</p>
-            <p>{comment.content}</p>
+            <p className="comment-user">{comment.user}</p>
+            <p className="comment-content">{comment.content}</p>
+            <hr />
             <p className="comment-date">{comment.date}</p>
           </div>
           <div className="comment-btns">
-            <button className="edit-btn" type="button">
+            <button  type="button">
               <img src="https://cdn-icons-png.flaticon.com/512/84/84380.png " alt="edit button" />
             </button>
             <button className="delete-btn" type="button">
