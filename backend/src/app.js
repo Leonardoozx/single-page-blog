98d8485b4b038ed620b207208dde8b49f96fb86a
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 18:39:28 -0300

    enabling cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const commentRoutes = require('./routes/comments.routes');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/comments', commentRoutes);
 
