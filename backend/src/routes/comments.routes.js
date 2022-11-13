abc986594e1f6fe1972741814783a2d77e3f8c71
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 16:43:00 -0300

    feat: implements all the comments routes

---
+++ backend/src/routes/comments.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+const CommentsController = require('../controller/comments.controller');
+
+const commentsController = new CommentsController()
+
+router.get('/', commentsController.getAllComments);
+router.post('/', commentsController.postNewComment)
+router.put('/:name', commentsController.updateCommentByName);
+router.delete('/:name', commentsController.deleteCommentByName)
+
+module.exports = router;
