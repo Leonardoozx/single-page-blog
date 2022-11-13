87fdb1f4a1ff44fe12d9d1cc2c28fe40ee5eb16f
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 19:11:40 -0300

    add more methods for the sendCorsMiddleware

--- backend/src/middlewares/sendCORSMiddleware.js
+++ backend/src/middlewares/sendCORSMiddleware.js
@@ -1,5 +1,6 @@
 const sendCORSMiddleware = (_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
   next();
 }
 
