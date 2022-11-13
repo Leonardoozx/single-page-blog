43a43de77a62aa1fedd3ca566988c177cf1379f3
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 16:43:14 -0300

    feat: a service for full comments table CRUD

---
+++ backend/src/service/comments.service.js
@@ -0,0 +1,49 @@
+const { Comments, sequelize } = require('../db/models');
+
+class CommentsServices {
+  formatedDate = new Date().toLocaleString().split(', ').join(' ');
+
+  getAllComments = () => Comments.findAll();
+
+  updateCommentByName = async (content, user) => {
+    try {
+      await Comments.update(
+        { content, date: this.formatedDate },
+        { where: { user } }
+      );
+      const updatedUserComment = {
+        user,
+        content,
+        date: this.formatedDate,
+      };
+      return { type: null, message: updatedUserComment };
+    } catch (_error) {
+      return { type: 'ERROR', message: 'Could not update your comment :(' };
+    }
+  };
+
+  postNewComment = async ({ content, user }) => {
+    try {
+      await Comments.create({ user, content, date: this.formatedDate });
+      const insertedComment = {
+        user,
+        content,
+        date: this.formatedDate,
+      };
+      return { type: null, message: insertedComment };
+    } catch (_error) {
+      return { type: 'ERROR', message: 'Could not insert your comment :(' };
+    }
+  };
+
+  deleteCommentByName = async (user) => {
+    try {
+      await Comments.destroy({ where: { user } });
+      return { type: null, message: '' };
+    } catch (_error) {
+      return { type: 'ERROR', message: 'Could not delete your comment :(' };
+    }
+  };
+}
+
+module.exports = CommentsServices;
