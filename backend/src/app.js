4ec4564ea6c7c14f8ea15d52f8b1196b16753730
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 18:43:57 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+
+app.use(cors({ origin: process.env.API_URL }));
 
 const app = express();
 
 const commentRoutes = require('./routes/comments.routes');
 
 app.use(express.json());
-app.use(cors());
 
 app.use('/comments', commentRoutes);
 
