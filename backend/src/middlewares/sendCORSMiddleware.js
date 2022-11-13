14996370ae411b720d85ae968ae644098bdfc4cc
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 19:15:02 -0300

    fix the CORS error

--- backend/src/middlewares/sendCORSMiddleware.js
+++ backend/src/middlewares/sendCORSMiddleware.js
@@ -1,7 +1,15 @@
 const sendCORSMiddleware = (_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
   next();
-}
+};
 
 module.exports = sendCORSMiddleware;
