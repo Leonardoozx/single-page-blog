da6fd2c9fb14f2ba4d9293365184c869c5dd8817
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 19:07:57 -0300

    fix cors error

--- backend/src/routes/comments.routes.js
+++ backend/src/routes/comments.routes.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const CommentsController = require('../controller/comments.controller');
+const sendCORSMiddleware = require('../middlewares/sendCORSMiddleware');
 
-const commentsController = new CommentsController()
+const commentsController = new CommentsController();
 
 router.use(sendCORSMiddleware);
 
 router.get('/', commentsController.getAllComments);
-router.post('/', commentsController.postNewComment)
+router.post('/', commentsController.postNewComment);
 router.put('/:name', commentsController.updateCommentByName);
-router.delete('/:name', commentsController.deleteCommentByName)
+router.delete('/:name', commentsController.deleteCommentByName);
 
 module.exports = router;
