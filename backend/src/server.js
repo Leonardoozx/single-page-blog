48b43c3d5cb0ec7138177eeecdb11ae138f3cff0
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 18:07:30 -0300

    refactor: changes the env var from PGPORT to PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 7248;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
