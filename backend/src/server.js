1c8439c83b56358b28c77851eb94ad0ee6c44a6b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-13 17:29:53 -0300

    trying to deploy the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-
-const PORT = process.env.PGPORT;
+const PORT = process.env.PGPORT || 3001;
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
