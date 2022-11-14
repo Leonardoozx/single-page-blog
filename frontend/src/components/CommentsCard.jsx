13d0f01707d94b94cf87895739e1a89d0f12d07e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-14 11:37:09 -0300

    now the blog only renders the date from comments

--- frontend/src/components/CommentsCard.jsx
+++ frontend/src/components/CommentsCard.jsx
@@ -1,13 +1,14 @@
 import CommentButtons from './CommentButtons'
 
 function CommentsCard({ comment }) {
+  const [date] = (comment.date).split('T');
   return (
     <div className="comment-container" key={comment.id}>
       <div className="comment-info">
         <p className="comment-user">{comment.user}</p>
         <p className="comment-content">{comment.content}</p>
         <hr />
-        <p className="comment-date">{comment.date}</p>
+        <p className="comment-date">{date}</p>
       </div>
       <div className="comment-btns">
         <CommentButtons {...comment} />
